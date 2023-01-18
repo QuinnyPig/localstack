8031c2dfc48ecf6a2def35d2b93cdcdbe73edf8d
Benjamin Simon <benjh.simon@gmail.com>
2023-01-18 16:48:14 +0100

    wip, debugging

--- localstack/aws/protocol/serializer.py
+++ localstack/aws/protocol/serializer.py
@@ -326,31 +326,35 @@ class ResponseSerializer(abc.ABC):
 
             # yield one event per generated event
             for event in event_generator:
+                print("event from event generator in serializer", event)
                 # find the actual event payload (the member with event=true)
-                event_member = None
-                for member in event_stream_shape_members.values():
-                    if member.serialization.get("event"):
-                        event_member = member
+                event_member_shape = None
+                event_member_name = None
+                for member_name, member_shape in event_stream_shape_members.items():
+                    if member_shape.serialization.get("event") and member_name in event:
+                        event_member_shape = member_shape
+                        event_member_name = member_name
                         break
-                if event_member is None:
+                if event_member_shape is None:
                     raise UnknownSerializerError("Couldn't find event shape for serialization.")
-
+                print("serializer shape/name", event_member_shape, event_member_name)
                 # serialize the part of the response for the event
                 self._serialize_response(
-                    event.get(event_member.name),
+                    event.get(event_member_name),
                     serialized_event_response,
-                    event_member,
-                    event_member.members if event_member is not None else None,
+                    event_member_shape,
+                    event_member_shape.members if event_member_shape is not None else None,
                     operation_model,
                     mime_type,
                 )
+                print("serialized response event", serialized_event_response.data)
                 # execute additional response traits (might be modifying the response)
                 serialized_event_response = self._prepare_additional_traits_in_response(
                     serialized_event_response, operation_model
                 )
                 # encode the event and yield it
                 yield self._encode_event_payload(
-                    event_type=event_member.name, content=serialized_event_response.data
+                    event_type=event_member_name, content=serialized_event_response.data
                 )
 
         return HttpResponse(
